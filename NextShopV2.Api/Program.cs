9523d12e854476ba1293be6aa2a0a2520b6bf631
Nguyen Thanh Dat <datnguyen9240@gmail.com>
2026-01-23 11:26:06 +0700

    fix: ssl

--- NextShopV2.Api/Program.cs
+++ NextShopV2.Api/Program.cs
@@ -77,7 +77,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
                 Database = uri.AbsolutePath.TrimStart('/'),
                 Username = userInfo.Length > 0 ? userInfo[0] : string.Empty,
                 Password = userInfo.Length > 1 ? userInfo[1] : string.Empty,
-                SslMode = SslMode.Require
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
             };
             options.UseNpgsql(npgBuilder.ConnectionString, o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
         }
