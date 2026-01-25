7ae8183916fe6a5f4d10f9d586beacfebff27798
Nguyen Thanh Dat <datnguyen9240@gmail.com>
2026-01-25 23:39:42 +0700

    fix: config for postgres

--- NextShopV2.Infrastructure/Persistence/AppDbContext.cs
+++ NextShopV2.Infrastructure/Persistence/AppDbContext.cs
@@ -160,9 +160,18 @@ namespace NextShopV2.Infrastructure.Persistence
             });
 
             modelBuilder.Entity<User>(eb => {
-                eb.HasIndex(u => u.Phone)
-                  .IsUnique()
-                  .HasFilter("[Phone] IS NOT NULL");
+                var index = eb.HasIndex(u => u.Phone).IsUnique();
+                
+                if (Database.IsSqlServer())
+                {
+                    index.HasFilter("[Phone] IS NOT NULL");
+                }
+                else if (Database.IsNpgsql())
+                {
+                    // Postgres allows multiple NULLs in unique index by default, 
+                    // but we can be explicit if needed.
+                    index.HasFilter("\"Phone\" IS NOT NULL");
+                }
             });
 
             base.OnModelCreating(modelBuilder);
