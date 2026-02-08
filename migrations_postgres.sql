9fd128a2d52c879a30f87ee9695cbb0d4a0ace47
Nguyen Thanh Dat <datnguyen9240@gmail.com>
2026-02-08 11:46:55 +0700

    fix: sql postgres

--- migrations_postgres.sql
+++ migrations_postgres.sql
@@ -208,6 +208,7 @@ CREATE TABLE "Orders" (
     "CancelledBy" text,
     "CancelledAt" timestamp with time zone,
     "SubTotal" numeric(18,0) NOT NULL,
+    "TaxAmount" numeric(18,2) NOT NULL DEFAULT 0,
     "DiscountAmount" numeric(18,0) NOT NULL,
     "TotalAmount" numeric(18,0) NOT NULL,
     "BuyerName" text,
